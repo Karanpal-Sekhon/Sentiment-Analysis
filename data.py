c2fdf84561a7c41c20478c6677ff679a42b0d077
Karanpal Sekhon <ksekhon2015@gmail.com>
2022-07-01 01:42:37 -0600

    Added choosing mechanism for movies

--- data.py
+++ data.py
@@ -8,10 +8,12 @@ from textblob import Word, TextBlob
 
 
 
-def main():
-    # DATA Extraction
-    r = requests.get(
-        "https://www.rottentomatoes.com/m/the_imitation_game/reviews")
+
+def grab_movie():
+    movie_name = input("Pick a movie: ")
+    movie_name = "_".join(movie_name.split())
+    movie_string_url = "https://www.rottentomatoes.com/m/" + movie_name + "/reviews"
+    r = requests.get(movie_string_url)
     soup = BeautifulSoup(r.text, "html.parser")
 
     # Grab reviews
@@ -20,43 +22,58 @@ def main():
     for result in results:
         reviews.append(result.text.strip())
 
-    # DATA Analysis
     df = pd.DataFrame(np.array(reviews), columns=["review"])
 
+    return df
+
+
+def data_anal(df):
     df["word_count"] = df["review"].apply(
-        lambda x: len(str(x).split(" ")))  # grabs word_count
+    lambda x: len(str(x).split(" ")))  # grabs word_count
     df["char_count"] = df["review"].str.len()  # grabs character count
     df["avg_word_length"] = df["review"].apply(lambda x: average_word(x))
     stop_words = stopwords.words("english")
     df["stopword_count"] = df["review"].apply(
         lambda x: len([x for x in x.split() if x in stop_words]))
+    return df
 
-    # Cleaning the Dataset
-    # print(df.columns.values)
+def clean_data(df):
     df["review_lower"] = df["review"].apply(
         lambda x: " ".join(x.lower() for x in x.split()))
     df["review_nopunc"] = df["review_lower"].str.replace(
         "[^\w\s]", "", regex=True)
+    stop_words = stopwords.words("english")
     df["cleaned_review"] = df["review_nopunc"].apply(lambda x: " ".join(x for x in x.split() if x not in stop_words))
     # add a clean rate later
+    return df
 
+
+def lem_sent(df):
     # Lemmatization
     # Greater -> Great 
     df["lemmatized"] = df["cleaned_review"].apply(lambda x: " ".join(Word(word).lemmatize() for word in x.split())) 
     # Sentiment Analysis
     df["polarity"] = df["lemmatized"].apply(lambda x: TextBlob(x).sentiment[0])
     df["subjectivity"] = df["lemmatized"].apply(lambda x: TextBlob(x).sentiment[1])
-
-    print(df.columns.values)
     df.drop(["review_lower", "review_nopunc", "cleaned_review", "lemmatized"], axis = 1, inplace=True)
-    print(df.columns.values)
-    print(df.head())
-
+    return df
 
 
 def average_word(review):
     words = review.split()
-    return sum(len(word) for word in words)
+    return sum(len(word) for word in words)   
+
+
+def main():
+    df = grab_movie()
+    df = data_anal(df)
+    df = clean_data(df)
+    df = lem_sent(df)
+
+
+
+
+
 
 
 if __name__ == "__main__":
